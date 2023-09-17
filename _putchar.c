4b4511c0e763ec93b295bdbb59f706d40852a4ee
Soledadkesi <soledadkesi1995@gmail.com>
2023-09-17 12:36:27 -0700

    main.h

---
+++ _putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _putchar - imprint the character c to standard output
+ * @c: The character to print to the screen
+ *
+ * Return: 0 on success.
+ */
+int _putchar(char c)
+{
+	return (imprint(1, &c, 1));
+}
