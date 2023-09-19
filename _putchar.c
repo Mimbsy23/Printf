4d9fcfad41d946f5ad0992cdf1338f01d076fd79
Soledadkesi <soledadkesi1995@gmail.com>
2023-09-19 08:46:51 -0700

    main.h

--- _putchar.c
+++ _putchar.c
@@ -1,38 +1,15 @@
+#include <unistd.h>
 #include "main.h"
-
 /**
-* _puts - prints the string on a new line
-* @str: this is the string to print
-*
-* Return:( str-a)
-*/
-int _puts(char *str)
-{
-char *a = str;/*declaration of variables*/
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
-while (*str)
-_putchar(*str++);
-return (str - a);
-}
 
-/**
-* _putchar - writes char c  to stdout
-* @c: char to print
-*
-* Return: On success 1 should be returned.
-* -1 is returned when error occurs and error is set appropriately.
-*/
-int _putchar(int c)
+int _putchar(char c)
 {
-static int i;
-static char buf[OUTPUT_BUF_SIZE];
-
-if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
-{
-write(1, buf, i);
-i = 0;
-}
-if (c != BUF_FLUSH)
-buf[i++] = c;
-return (1);
+	return (write(1, &c, 1));
 }
